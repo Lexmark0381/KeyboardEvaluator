738a803f339779400e40847f3c4d5b00210b9b04
Piermarco Barbè <barbep@usi.ch>
2017-11-12 17:46:03 +0100

    diff_match_patch is on npm, wow

--- KeyboardEvaluator/app.js
+++ KeyboardEvaluator/app.js
@@ -1,17 +1,33 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var DiffMatchPatch = require('diff-match-patch');
+
+var dmp = new DiffMatchPatch();
+
+// var diff_match_patch_test = require("./diff_match_patch_test.js")
+
+
+var x =  dmp.diff_main;
+
+var correctString = "Augusta Ada King-Noel, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine"
+
+console.log(typeof x)
+console.log(x)
+
+// var dmp = new diff_match_patch();
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-
     socket.on('text', function (data) {
         // console.log(data);
-        history.push(data);
+        // history.push(data);
         // console.log(history)
+        listAnalyzer(data)
     })
 
 });
