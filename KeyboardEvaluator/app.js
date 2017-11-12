736c0c5b914fc83ba85d5b9180402f573f2eac55
Piermarco Barbè <barbep@usi.ch>
2017-11-12 18:24:01 +0100

    new server

--- KeyboardEvaluator/app.js
+++ KeyboardEvaluator/app.js
@@ -5,31 +5,20 @@ var DiffMatchPatch = require('diff-match-patch');
 
 var dmp = new DiffMatchPatch();
 
-// var diff_match_patch_test = require("./diff_match_patch_test.js")
-
-
-var x =  dmp.diff_main;
 
 var correctString = "Augusta Ada King-Noel, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine"
-
-console.log(typeof x)
-console.log(x)
-
-// var dmp = new diff_match_patch();
-
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    socket.on('text', function (data) {
-        // console.log(data);
-        // history.push(data);
-        // console.log(history)
-        listAnalyzer(data)
+    socket.on('userText', function (text) {
+        var timeout = 1.0;
+        var d = dmp.diff_main(correctString, text)
+        var html = dmp.diff_prettyHtml(d);
+        socket.emit('HTMLdiff', html);
     })
-
 });
 
 http.listen(3000, function(){
